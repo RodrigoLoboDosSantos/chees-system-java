1dd31e4b2e42f02fc37215480e78c677ade426af
Rodrigo Lobo <lobo_773@hotmail.com>
2022-05-12 17:23:21 -0300

    Implementing possible moves of Rook

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -18,7 +18,7 @@ public class King extends ChessPiece{
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
-		return null;
+		return mat;
 	}
 
 }
