c17a4c57d7ebcbbd4f34f29596b9288c67169e7d
Rodrigo Lobo <lobo_773@hotmail.com>
2022-05-18 18:22:50 -0300

    Special move - Castling

--- src/chess/pieces/Knight.java
+++ src/chess/pieces/Knight.java
@@ -10,12 +10,12 @@ public class Knight extends ChessPiece{
 	public Knight(Board board, Color color) {
 		super(board, color);
 			}
-	
+
 	@Override
 	public String toString() {
 		return "N";
 	}
-	
+
 	private boolean canMove(Position position) {
 		ChessPiece p = (ChessPiece)(getBoard().piece(position));
 		return p == null || p.getColor() != getColor();
@@ -24,9 +24,9 @@ public class Knight extends ChessPiece{
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
-		
+
 		Position p = new Position(0, 0);
-		
+
 
 		p.setValues(position.getRow() - 1, position.getColumn() -2 );
 		if (getBoard().positionExists(p) && canMove(p)) {
